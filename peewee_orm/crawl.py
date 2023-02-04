234736b9682498d261c6b96326a16a41790672cc
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-02-05 00:10:08 +0330

    write func get_links() in crawl.py and get all link from site and save links in database in main.py in run() func

--- peewee_orm/crawl.py
+++ peewee_orm/crawl.py
@@ -2,10 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def start():
-    url = 'https://www.trthaber.com/haber/spor/' \
-         'fenerbahce-taraftari-tffyi-protesto-etti-743533.html'
-
+def crawl_page(url):
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -15,5 +12,26 @@ def start():
         print(title.text)
 
 
+def get_links():
+    response = requests.get('https://www.trthaber.com/haber/spor/')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = []
+    if response.status_code == 200:
+        for link in soup.find_all('a'):
+            href = link.attrs.get("href")
+            if href is not None and href.startswith('haber/spor/'):
+                links.append("https://www.trthaber.com/"+href)
+    return links
+
+
 if __name__ == '__main__':
-    start()
+
+    #  links = ['https://www.trthaber.com/haber/spor/fenerbahce-taraftari-tffyi-protesto-etti-743533.html',
+    #          'https://www.trthaber.com/haber/spor/manchester-united-zirve-takibini-surdurdu-743532.html',
+    #          'https://www.trthaber.com/haber/spor/galatasaray-trabzonspor-macina-hazir-743522.html',
+    #          'https://www.trthaber.com/haber/spor/bandirmasporda-mesut-bakkalin-sozlesmesi-feshedildi-743514.html',
+    #          ]
+    # for link in links:
+    #     crawl_page(link)
+    get_links()
+
--- peewee_orm/main.py
+++ peewee_orm/main.py
@@ -1,11 +1,16 @@
 import sys
 from utils.db import create_table
 from models import Article, Category
+from crawl import get_links
 
 
 def run():
+    links = get_links()
+    cat = Category.create(name="sport")
+    for link in links:
+        article = Article.create(url=link, category=cat)
+        print(article.id)
 
-    print(f'Hi')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
